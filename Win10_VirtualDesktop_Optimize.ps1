433739acd52b3b8064388ed54849786925487f51
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2020-09-16 17:24:56 -0500

    First commit, fix disable scheduled task and add logging
    Changed the code to disable scheduled tasks and adding error logging to each section.  Outputs a .txt log file (script deletes .log files) with error and any other logged information.

--- Win10_VirtualDesktop_Optimize.ps1
+++ Win10_VirtualDesktop_Optimize.ps1
@@ -1,4 +1,4 @@
-﻿<#####################################################################################################################################
+<#####################################################################################################################################
 
     This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.  
     THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, 
@@ -29,7 +29,7 @@
 Param
 (
     [Parameter()]
-    [ValidateSet('1909','2004')]
+    [ValidateSet('1909', '2004')]
     $WindowsVersion = 2004,
 
     [Parameter()]
@@ -81,57 +81,87 @@ The Store and a few others, such as Wallet, were left off intentionally.  Though
 it is nearly impossible to get it back.  Please review the lists below and comment out or remove references to packages that you do not want to remove.
 #>
 
+
+
 $StartTime = Get-Date
 $CurrentLocation = Get-Location
 
-Try 
-{
+Try {
     Set-Location (Join-Path $PSScriptRoot $WindowsVersion)-ErrorAction Stop
 }
-Catch
-{
+Catch {
     Write-Warning "Invalid Path $(Join-Path $PSScriptRoot $WindowsVersion) - Exiting script!"
     Break
 }
 
+#region Set logging 
+
+#Checks if the log file exists and creates it if not
+
+
+$logFile = $CurrentLocation.path + "\" + (get-date -format 'yyyyMMdd') + '_VirtualDesktopOptimizeLog.txt'
+function Write-Log {
+    Param($message)
+    Write-Output "$(get-date -format 'MM/dd/yy HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
+}
+
+#endregion
+
 #region Disable, then remove, Windows Media Player including payload
-    
-Try
-{
-    Write-Verbose "Disabling Windows Media Player Feature"
-    Disable-WindowsOptionalFeature -Online -FeatureName WindowsMediaPlayer -NoRestart | Out-Null
-    Get-WindowsPackage -Online -PackageName "*Windows-mediaplayer*" | ForEach-Object { 
-        Write-Verbose "Removeing $($_.PackageName)"
-        Remove-WindowsPackage -PackageName $_.PackageName -Online -ErrorAction SilentlyContinue  -NoRestart | Out-Null
+
+Write-Log "### Disabling Windows Media Player Feature ###"
+Try {
+    Disable-WindowsOptionalFeature -ErrorAction stop -Online -FeatureName WindowsMediaPlayer -NoRestart | Out-Null
+    Get-WindowsPackage -Online -ErrorAction Stop -PackageName "*Windows-mediaplayer*" | ForEach-Object { 
+        try {
+            Write-Log "Removeing $($_.PackageName)"
+            Remove-WindowsPackage -PackageName $_.PackageName -ErrorAction Stop -Online   -NoRestart | Out-Null
+        }
+        catch {
+            $ErrorMessage = $_.Exception.message
+            write-log "Error removing Windows Media Player feature: $ErrorMessage"
+        }
     }
 }
-Catch { }
+Catch { 
+    $ErrorMessage = $_.Exception.message
+    write-log "Error disabling Windows Media Player feature: $ErrorMessage"
+}
 
 #endregion
 
 #region Begin Clean APPX Packages
 
-
-If (Test-Path .\ConfigurationFiles\AppxPackages.json)
-{
+Write-Log "### Begin clean APPX Packages ###"
+If (Test-Path .\ConfigurationFiles\AppxPackages.json) {
     $AppxPackage = Get-Content .\ConfigurationFiles\AppxPackages.json | ConvertFrom-Json 
     $AppxPackage = $AppxPackage | Where-Object { $_.VDIState -eq 'Disabled' }
 }
+Else {
+    Write-Log "Can not find AppxPackages.json"
+}
 
-If ($AppxPackage.Count -gt 0)
-{
-    Foreach ($Item in $AppxPackage)
-    {
-        $Package = "*$($Item.AppxPackage)*"
-        Write-Verbose "Attempting to remove $($Item.AppxPackage) - $($Item.Description)"
-        Get-AppxPackage -Name $Package | Remove-AppxPackage -ErrorAction SilentlyContinue  | Out-Null
+If ($AppxPackage.Count -gt 0) {
+    Foreach ($Item in $AppxPackage) {
+        try {
+            $Package = "*$($Item.AppxPackage)*"
+            Write-Log "Attempting to remove $($Item.AppxPackage) - $($Item.Description)"
+            Get-AppxPackage -Name $Package | Remove-AppxPackage -ErrorAction Stop  | Out-Null
         
-        Write-Verbose "Attempting to remove [All Users] $($Item.AppxPackage) - $($Item.Description)"
-        Get-AppxPackage -AllUsers -Name $Package | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue 
+            Write-Log "Attempting to remove [All Users] $($Item.AppxPackage) - $($Item.Description)"
+            Get-AppxPackage -AllUsers -Name $Package | Remove-AppxPackage -AllUsers -ErrorAction Stop 
         
-        Write-Verbose "Removing Provisioned Package $($item.AppxPackage)"
-        Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -like $Package } | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Out-Null
+            Write-Log "Removing Provisioned Package $($item.AppxPackage)"
+            Get-AppxProvisionedPackage -Online | Where-Object { $_.PackageName -like $Package } | Remove-AppxProvisionedPackage -Online -ErrorAction Stop | Out-Null
+        }
+        Catch {
+            $ErrorMessage = $_.Exception.message
+            write-log "Error removing Appx Package: $Package $ErrorMessage"
+        }
     }
+}
+Else {
+    Write-Log "No AppxPackages.json file found"
 }
 #endregion
 
@@ -139,20 +169,28 @@ If ($AppxPackage.Count -gt 0)
 
 # This section is for disabling scheduled tasks.  If you find a task that should not be disabled
 # change its "VDIState" from Disabled to Enabled, or remove it from the json completely.
-If (Test-Path .\ConfigurationFiles\ScheduledTasks.json)
-{
+
+Write-Log "### Begin disable scheduled tasks ###"
+If (Test-Path .\ConfigurationFiles\ScheduledTasks.json) {
     $SchTasksList = Get-Content .\ConfigurationFiles\ScheduledTasks.json | ConvertFrom-Json
     $SchTasksList = $SchTasksList | Where-Object { $_.VDIState -eq 'Disabled' }
 }
-If ($SchTasksList.count -gt 0)
-{
-    #$EnabledScheduledTasks = Get-ScheduledTask | Where-Object { $_.State -ne "Disabled" }
-    Foreach ($Item in $SchTasksList)
-    {
-        #$Task = (($Item -split ":")[0]).Trim()
-        Write-Verbose "Disabling Scheduled Task $($Item.ScheduledTask)"
-        Disable-ScheduledTask -TaskName $Item.ScheduledTask -ErrorAction SilentlyContinue
-        #$EnabledScheduledTasks | Where-Object { $_.TaskName -like "*$Task*" } #| Disable-ScheduledTask
+Else {
+    Write-Log "No ScheduledTask.json file found"
+}
+
+If ($SchTasksList.count -gt 0) {
+    Foreach ($Item in $SchTasksList) {
+        Try {
+            $TaskObject = Get-ScheduledTask $Item.ScheduledTask
+            Write-Log "Disabling Scheduled Task $($Item.ScheduledTask)"
+            Disable-ScheduledTask -InputObject $TaskObject -ErrorAction Stop | Out-Null
+        }
+        Catch {
+            $ErrorMessage = $_.Exception.message
+            $TaskName = $item.ScheduledTask
+            write-log "Error disabling Scheduled Task $($item.ScheduledTask): $Package $ErrorMessage"
+        }
     }
 }
 #endregion
@@ -160,34 +198,56 @@ If ($SchTasksList.count -gt 0)
 #region Customize Default User Profile
 # Apply appearance customizations to default user registry hive, then close hive file
 
-If (Test-Path .\ConfigurationFiles\DefaultUserSettings.txt)
-{
+Write-Log "### Begin customize default user profile ###"
+If (Test-Path .\ConfigurationFiles\DefaultUserSettings.txt) {
     $DefaultUserSettings = Get-Content .\ConfigurationFiles\DefaultUserSettings.txt
 }
-If ($DefaultUserSettings.count -gt 0)
-{
-    Write-Verbose "Processing Default User Settings registry keys"
-    Foreach ($Item in $DefaultUserSettings)
-    {
-        Start-Process C:\Windows\System32\Reg.exe -ArgumentList "$Item" -Wait 
+Else {
+    Write-Log "No DefaultUserSettings.txt file found"
+}
+If ($DefaultUserSettings.count -gt 0) {
+    Write-Log "Processing Default User Settings registry keys"
+    Foreach ($Item in $DefaultUserSettings) {
+        Try {
+            Write-Log "Processing Settings registry keys: $Item"
+            Start-Process C:\Windows\System32\Reg.exe -ErrorAction Stop -ArgumentList "$Item" -Wait 
+        }
+        Catch {
+            $ErrorMessage = $_.Exception.message
+            write-log "Error applying appearance customizations: $ErrorMessage"
+        }
     }
+}
+Else {
+    Write-Log "No appearance customizations to apply"
 }
 #endregion
 
 #region Disable Windows Traces
-If (Test-Path .\ConfigurationFiles\Autologgers.Json)
-{
+
+Write-Log "### Begin disable Windows Traces ###"
+If (Test-Path .\ConfigurationFiles\Autologgers.Json) {
     $DisableAutologgers = Get-Content .\ConfigurationFiles\Autologgers.Json | ConvertFrom-Json
 }
+Else {
+    Write-Log "No Autolaggers.json file found"
+}
 
-If ($DisableAutologgers.count -gt 0)
-{
+If ($DisableAutologgers.count -gt 0) {
     $DisableAutologgers = $DisableAutologgers | Where-Object { $_.Disabled -eq 'True' }
-    Foreach ($Item in $DisableAutologgers)
-    {
-        Write-Verbose "Adding $($Item.KeyName)"
-        New-ItemProperty -Path "$($Item.KeyName)" -Name "Start" -PropertyType "DWORD" -Value "0" -Force | Out-Null
+    Foreach ($Item in $DisableAutologgers) {
+        try {
+            Write-Log "Adding $($Item.KeyName)"
+            New-ItemProperty -Path "$($Item.KeyName)" -Name "Start" -PropertyType "DWORD" -Value "0" -Force -ErrorAction Stop | Out-Null
+        }
+        Catch {
+            $ErrorMessage = $_.Exception.message
+            Write-Log "Error disabling Windows Trace $($Item.KeyName) : $ErrorMessage"
+        }
     }
+}
+Else {
+    Write-Log "No Windows traces to disable"
 }
 #endregion
 
@@ -198,45 +258,73 @@ If ($DisableAutologgers.count -gt 0)
 #   * change the "Enable Windows NTP Client" setting.
 #   * set the "Select when Quality Updates are received" policy
 
-if (Test-Path (Join-Path $PSScriptRoot "LGPO\LGPO.exe")) 
-{
-    Write-Verbose "Importing Local Group Policy Items"
-    Start-Process (Join-Path $PSScriptRoot "LGPO\LGPO.exe") -ArgumentList "/g .\LGPO" -Wait
+Write-Log "### Begin local Group Policy settings ###"
+if (Test-Path (Join-Path $PSScriptRoot "LGPO\LGPO.exe")) {
+    Try {
+        Write-Log "Importing Local Group Policy Items"
+        Start-Process -ErrorAction Stop (Join-Path $PSScriptRoot "LGPO\LGPO.exe") -ArgumentList "/g .\LGPO" -Wait
+    }
+    Catch {
+        $ErrorMessage = $_.Exception.message
+        Write-Log "Error setting the Local Group Policy Settings: $ErrorMessage"
+    }
+}
+Else {
+    Write-Log "Error setting Local Group Policy: No LGPO.exe file found"
 }
 #endregion
 
 #region Disable Services
-If (Test-Path .\ConfigurationFiles\Services.json)
- 
-{
+
+Write-Log "### Begin disable services ###"
+If (Test-Path .\ConfigurationFiles\Services.json) {
     $ServicesToDisable = Get-Content .\ConfigurationFiles\Services.json | ConvertFrom-Json
 }
+Else {
+    Write-Log "No Services.json file found"
+}
 
-If ($ServicesToDisable.count -gt 0)
-{
+If ($ServicesToDisable.count -gt 0) {
     $ServicesToDisable = $ServicesToDisable | Where-Object { $_.VDIState -eq 'Disabled' }
-    Foreach ($Item in $ServicesToDisable)
-    {
-        Write-Verbose "Stopping $($Item.Name) - $($Item.Description)"
-        Stop-Service $Item.Name -Force -ErrorAction SilentlyContinue
-        Write-Verbose "`t`tDisabling $($Item.Name)"
-        Set-Service $Item.Name -StartupType Disabled 
+    Foreach ($Item in $ServicesToDisable) {
+        Try {
+            Write-Log "Stopping $($Item.Name) - $($Item.Description)"
+            Stop-Service $Item.Name -Force -ErrorAction Stop
+            Write-Log "`t`tDisabling $($Item.Name)"
+            Set-Service $Item.Name -StartupType Disabled  -ErrorAction Stop
+        }
+        Catch {
+            $ErrorMessage = $_.Exception.message
+            Write-Log "Error disabling service $($Item.name): $ErrorMessage"
+        }
     }
 }
 #endregion
 
 #region Network Optimization
 # LanManWorkstation optimizations
-Write-Verbose "Configuring LanManWorlstation Optimizations"
-New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "DisableBandwidthThrottling" -PropertyType "DWORD" -Value "1" -Force | Out-Null
-New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "FileInfoCacheEntriesMax" -PropertyType "DWORD" -Value "1024" -Force | Out-Null
-New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "DirectoryCacheEntriesMax" -PropertyType "DWORD" -Value "1024" -Force | Out-Null
-New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "FileNotFoundCacheEntriesMax" -PropertyType "DWORD" -Value "1024" -Force | Out-Null
-New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "DormantFileLimit" -PropertyType "DWORD" -Value "256" -Force | Out-Null
+Write-Log "### Configuring LanManWorkstation Optimizations ###"
+Try {
+    New-ItemProperty -ErrorAction Stop -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "DisableBandwidthThrottling" -PropertyType "DWORD" -Value "1" -Force | Out-Null
+    New-ItemProperty -ErrorAction Stop -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "FileInfoCacheEntriesMax" -PropertyType "DWORD" -Value "1024" -Force | Out-Null
+    New-ItemProperty -ErrorAction Stop -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "DirectoryCacheEntriesMax" -PropertyType "DWORD" -Value "1024" -Force | Out-Null
+    New-ItemProperty -ErrorAction Stop -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "FileNotFoundCacheEntriesMax" -PropertyType "DWORD" -Value "1024" -Force | Out-Null
+    New-ItemProperty -ErrorAction Stop -Path "HKLM:\System\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "DormantFileLimit" -PropertyType "DWORD" -Value "256" -Force | Out-Null
+}
+Catch {
+    $ErrorMessage = $_.Exception.message
+    Write-Log "Error setting Network Optimization: $ErrorMessage"
+}
 
 # NIC Advanced Properties performance settings for network biased environments
-Write-Verbose "Configuring Network Adapter Buffer Size"
-Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB
+Write-Log "### Configuring Network Adapter Buffer Size ###"
+Try {
+    Set-NetAdapterAdvancedProperty -ErrorAction Stop -DisplayName "Send Buffer Size" -DisplayValue 4MB
+}
+Catch {
+    $ErrorMessage = $_.Exception.message
+    Write-Log "Error setting NIC Send Buffer Size: $ErrorMessage"
+}
 
 <# Note that the above setting is for a Microsoft Hyper-V VM.  You can adjust these values in your environment...
 by querying in PowerShell using Get-NetAdapterAdvancedProperty, and then adjusting values using the...
@@ -248,36 +336,98 @@ Set-NetAdapterAdvancedProperty command.
 # ADDITIONAL DISK CLEANUP
 # Delete not in-use files in locations C:\Windows\Temp and %temp%
 # Also sweep and delete *.tmp, *.etl, *.evtx, *.log, *.dmp, thumbcache*.db (not in use==not needed)
+# Expect errors in the log for file in use
 # 5/18/20: Removing Disk Cleanup and moving some of those tasks to the following manual cleanup
 
-Write-Verbose "Removing .tmp, .etl, .evtx, thumbcache*.db, *.log files not in use"
-Get-ChildItem -Path c:\ -Include *.tmp, *.dmp, *.etl, *.evtx, thumbcache*.db, *.log -File -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -ErrorAction SilentlyContinue
+Write-Log "### Removing .tmp, .etl, .evtx, thumbcache*.db, *.log files not in use ###"
+$ChildFileItems = Get-ChildItem -Path c:\ -Include *.tmp, *.dmp, *.etl, *.evtx, thumbcache*.db, *.log -File -Recurse -Force -ErrorAction SilentlyContinue 
+
+ForEach ($Item in $ChildFileItems) {
+    Try {
+        # The next line may be too much data to log
+        write-log "Deleting: $Item"
+        remove-item $Item -ErrorAction Stop
+    }
+    Catch {
+        $ErrorMessage = $_.Exception.message
+        Write-Log "Error removing files not in use: $ErrorMessage"
+    }
+}
 
 # Delete "RetailDemo" content (if it exits)
-Write-Verbose "Removing Retail Demo content (if it exists)"
-Get-ChildItem -Path $env:ProgramData\Microsoft\Windows\RetailDemo\* -Recurse -Force -ErrorAction SilentlyContinue | Remove-Item -Recurse -ErrorAction SilentlyContinue
+Write-Log "### Removing Retail Demo content (if it exists) ###"
+$ChildRetailItems = Get-ChildItem -Path $env:ProgramData\Microsoft\Windows\RetailDemo\* -Recurse -Force -ErrorAction SilentlyContinue 
+foreach ($item in $ChildRetailItems) {
+    Try {
+        remove-item $item -ErrorAction Stop -force
+    }
+    Catch {
+        $ErrorMessage = $_.Exception.message
+        Write-Log "Error removing RetailDemo Content: $ErrorMessage"
+    }
+}
 
 # Delete not in-use anything in the C:\Windows\Temp folder
-Write-Verbose "Removing all files not in use in $env:windir\TEMP"
-Remove-Item -Path $env:windir\Temp\* -Recurse -Force -ErrorAction SilentlyContinue
+Write-Log "### Removing all files not in use in $env:windir\TEMP ###"
+$ChildTempItems = Get-ChildItem -Path $env:windir\Temp\* -Recurse -Force -ErrorAction SilentlyContinue
+foreach ($item in $ChildTempItems) {
+    Try {
+        remove-item $item -ErrorAction Stop -force
+    }
+    Catch {
+        $ErrorMessage = $_.Exception.message
+        Write-Log "Error removing RetailDemo Content: $ErrorMessage"
+    }
+}
+
 
 # Clear out Windows Error Reporting (WER) report archive folders
-Write-Verbose "Cleaning up WER report archive"
-Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\Temp\* -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\ReportArchive\* -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item -Path $env:ProgramData\Microsoft\Windows\WER\ReportQueue\* -Recurse -Force -ErrorAction SilentlyContinue
+Write-Log "### Cleaning up WER report archive ###"
+$WerPath = @(
+    "\Microsoft\Windows\WER\Temp\*"
+    "\Microsoft\Windows\WER\ReportArchive\*"
+    "\Microsoft\Windows\WER\ReportQueue\*"
+)
+foreach ($Item in $WerPath) {
+    Try {
+        Remove-Item -path "$env:ProgramData$Item" -Recurse -Force -ErrorAction Stop
+    }
+    Catch {
+        $ErrorMessage = $_.Exception.message
+        Write-Log "Error removing WER Report Archive: $ErrorMessage"
+    }
+}
+
 
 # Delete not in-use anything in your %temp% folder
-Write-Verbose "Removing files not in use in $env:TEMP directory"
-Remove-Item -Path $env:TEMP\* -Recurse -Force -ErrorAction SilentlyContinue
+Write-Log "### Removing files not in use in $env:TEMP directory ###"
+Try {
+Remove-Item -Path $env:TEMP\* -Recurse -Force -ErrorAction Stop
+}
+Catch {
+    $ErrorMessage = $_.Exception.message
+    Write-Log "Error removing files in the TEMP directory: $ErrorMessage"
+}
 
 # Clear out ALL visible Recycle Bins
-Write-Verbose "Clearing out ALL Recycle Bins"
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+Write-Log "### Clearing out ALL Recycle Bins ###"
+Try{
+Clear-RecycleBin -Force -ErrorAction Stop
+}
+Catch {
+    $ErrorMessage = $_.Exception.message
+    Write-Log "Error Clearing the Recycle Bins: $ErrorMessage"
+}
 
 # Clear out BranchCache cache
-Write-Verbose "Clearing BranchCache cache"
-Clear-BCCache -Force -ErrorAction SilentlyContinue
+Write-Log "### Clearing BranchCache cache ###"
+Try{
+Clear-BCCache -Force -ErrorAction Stop
+}
+Catch {
+    $ErrorMessage = $_.Exception.message
+    Write-Log "Error removing BranchCache: $ErrorMessage"
+}
 
 #endregion
 
@@ -286,12 +436,10 @@ $EndTime = Get-Date
 $ScriptRunTime = New-TimeSpan -Start $StartTime -End $EndTime
 Write-Host "Total Run Time: $($ScriptRunTime.Hours) Hours $($ScriptRunTime.Minutes) Minutes $($ScriptRunTime.Seconds) Seconds" -ForegroundColor Cyan
 
-If ($Restart)
-{
+If ($Restart) {
     Restart-Computer -Force
 }
-else 
-{
+else {
     Write-Warning "A reboot is required for all changed to take effect"
 }
 #Add-Type -AssemblyName PresentationFramework
