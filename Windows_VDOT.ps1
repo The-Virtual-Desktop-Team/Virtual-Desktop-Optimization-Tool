cf84f0aaa012cf0f9aa92f00c163fd405a9b92de
Robert Smith <61853161+robsmi-msfte@users.noreply.github.com>
2023-04-14 17:49:18 -0400

    Updating OneDrive removal
    Need to be more thorough on removing OneDrive.  I just ran VDOT, checked it after Sysprep, and a OneDrive .lnk file was back.

--- Windows_VDOT.ps1
+++ Windows_VDOT.ps1
@@ -694,18 +694,7 @@ PROCESS {
     {
         Write-EventLog -EventId 80 -Message "Remove OneDrive Commercial" -LogName 'Virtual Desktop Optimization' -Source 'AdvancedOptimizations' -EntryType Information
         Write-Host "[VDI Advanced Optimize] Removing OneDrive Commercial" -ForegroundColor Cyan
-        $OneDrivePath = @('C:\Windows\System32\OneDriveSetup.exe', 'C:\Windows\SysWOW64\OneDriveSetup.exe')   
-        $OneDrivePath | foreach {
-            If (Test-Path $_)
-            {
-                Write-Host "`tAttempting to uninstall $_"
-                Write-EventLog -EventId 80 -Message "Commercial $_" -LogName 'Virtual Desktop Optimization' -Source 'AdvancedOptimizations' -EntryType Information
-                Start-Process $_ -ArgumentList "/uninstall" -Wait
-            }
-        }
-        Write-EventLog -EventId 80 -Message "Removing shortcut links for OneDrive" -LogName 'Virtual Desktop Optimization' -Source 'AdvancedOptimizations' -EntryType Information
-        Remove-Item -Path 'C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk' -Force -ErrorAction SilentlyContinue
-        Remove-Item -Path 'C:\Windows\ServiceProfiles\NetworkService\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk'  -Force -ErrorAction SilentlyContinue
+        Get-ChildItem C:\OneDrive*.* -Recurse -ErrorAction SilentlyContinue | Remove-Item -Force -ErrorAction SilentlyContinue
     }
 
     #endregion
