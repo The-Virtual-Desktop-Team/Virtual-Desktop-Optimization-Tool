5c2da4f352233df9a71e6a827a597a6ee4d2034f
Robert Smith <61853161+robsmi-msfte@users.noreply.github.com>
2023-04-17 11:17:10 -0400

    Update "remove OneDrive" code
    Addressing an issue where the current VDOT "remove OneDrive" code was run, and it worked as intended.  Yet OneDrive still shows up on the Start menu.  It was not functional of course, but where customers ask for OneDrive to be removed, they don't want it right there on the Start menu.  This code does a more sweeping deletion of OneDrive files.
    And it's not like you can't get it back if you want it.  You can download the OneDrive installer.

--- Windows_VDOT.ps1
+++ Windows_VDOT.ps1
@@ -703,9 +703,8 @@ PROCESS {
                 Start-Process $_ -ArgumentList "/uninstall" -Wait
             }
         }
-        Write-EventLog -EventId 80 -Message "Removing shortcut links for OneDrive" -LogName 'Virtual Desktop Optimization' -Source 'AdvancedOptimizations' -EntryType Information
-        Remove-Item -Path 'C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk' -Force -ErrorAction SilentlyContinue
-        Remove-Item -Path 'C:\Windows\ServiceProfiles\NetworkService\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk'  -Force -ErrorAction SilentlyContinue
+        Write-EventLog -EventId 80 -Message "Removing shortcut links and other OneDrive collateral." -LogName 'Virtual Desktop Optimization' -Source 'AdvancedOptimizations' -EntryType Information
+        Get-ChildItem C:\OneDrive.* -ErrorAction SilentlyContinue -Recurse | Remove-Item -Force -ErrorAction SilentlyContinue
     }
 
     #endregion
