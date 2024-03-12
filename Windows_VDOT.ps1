ebce398f626a600ff912426f63afff219da1aea0
Dave <57664345+dgiesselbach@users.noreply.github.com>
2024-03-12 09:14:39 +0100

    Update Windows_VDOT.ps1
    change -EntryType Error back to Information

--- Windows_VDOT.ps1
+++ Windows_VDOT.ps1
@@ -592,7 +592,7 @@ PROCESS {
                             }
                             Else
                             {
-                                Write-EventLog -EventId 80 -Message "Error: Creating Name $($Key.RegItemValueName), Value $($Key.RegItemValue) and Path $($Key.RegItemPath)" -LogName 'Virtual Desktop Optimization' -Source 'LGPO' -EntryType Error
+                                Write-EventLog -EventId 80 -Message "Error: Creating Name $($Key.RegItemValueName), Value $($Key.RegItemValue) and Path $($Key.RegItemPath)" -LogName 'Virtual Desktop Optimization' -Source 'LGPO' -EntryType Information
                                 Write-Verbose "Error: Creating Name $($Key.RegItemValueName), Value $($Key.RegItemValue) and Path $($Key.RegItemPath)"
                                 New-Item -Path $Key.RegItemPath -Force | New-ItemProperty -Name $Key.RegItemValueName -PropertyType $Key.RegItemValueType -Value $Key.RegItemValue -Force | Out-Null
                             }
